8a6e4dc4ad8b49cc701d9737e0f39d57e83df262
dieginin <diegobalestra9@gmail.com>
2026-02-01 01:32:06 -0800

    feat: add bcrypt for password hashing and enhance user registration validation

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,8 @@
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { LoginUserDto, RegisterUserDto, UpdateUserDto } from './dto';
-
-import { Injectable } from '@nestjs/common';
+import { hashSync } from 'bcrypt';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { User } from './entities/user.entity';
 
 @Injectable()
@@ -12,12 +12,33 @@ export class AuthService {
     private readonly usersRepository: Repository<User>,
   ) {}
 
+  private async checkUserExistence(email: string, username: string) {
+    if (await this.usersRepository.findOneBy({ email: email.toLowerCase() }))
+      throw new ConflictException('An user with this email already exists');
+    if (
+      await this.usersRepository.findOneBy({ username: username.toLowerCase() })
+    )
+      throw new ConflictException('An user with this username already exists');
+  }
+
   login(loginUserDto: LoginUserDto) {
     return loginUserDto;
   }
 
-  register(registerUserDto: RegisterUserDto) {
-    return registerUserDto;
+  async register(registerUserDto: RegisterUserDto) {
+    const { email, username } = registerUserDto;
+    await this.checkUserExistence(email, username);
+
+    const { password, ...userData } = registerUserDto;
+    const user = this.usersRepository.create({
+      ...userData,
+      password: hashSync(password, 10),
+    });
+    await this.usersRepository.save(user);
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { password: _, ...userWithoutPassword } = user;
+    return userWithoutPassword;
   }
 
   update(id: string, updateUserDto: UpdateUserDto) {
--- src/auth/dto/register-user.dto.ts
+++ src/auth/dto/register-user.dto.ts
@@ -1,1 +1,37 @@
-export class RegisterUserDto {}
+import {
+  IsArray,
+  IsEmail,
+  IsOptional,
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
+export class RegisterUserDto {
+  @IsString()
+  @MinLength(1)
+  username: string;
+
+  @IsString()
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @MinLength(1)
+  fullName: string;
+
+  @IsString()
+  @MinLength(6)
+  @MaxLength(50)
+  @Matches(/(?:(?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message:
+      'The password must have a Uppercase, lowercase letter and a number',
+  })
+  password: string;
+
+  @IsString({ each: true })
+  @IsArray()
+  @IsOptional()
+  roles?: string[];
+}
