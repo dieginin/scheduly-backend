444f8695a972564f913b23d143522d45c054209d
dieginin <diegobalestra9@gmail.com>
2026-02-01 00:32:10 -0800

    feat add TypeOrm and .env config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,22 @@
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
-@Module({ imports: [AuthModule] })
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      autoLoadEntities: true,
+      database: process.env.DB_NAME,
+      host: process.env.DB_HOST,
+      password: process.env.DB_PASSWORD,
+      port: Number(process.env.DB_PORT),
+      synchronize: process.env.NODE_ENV === 'developments',
+      type: 'postgres',
+      username: process.env.DB_USERNAME,
+    }),
+    AuthModule,
+  ],
+})
 export class AppModule {}
