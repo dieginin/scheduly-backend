c85ac46a8d997b6cd6ebf4f96437ca298de12dfe
dieginin <diegobalestra9@gmail.com>
2026-02-01 00:32:41 -0800

    feat: implement User entity with fields and normalization methods

--- src/auth/entities/user.entity.ts
+++ src/auth/entities/user.entity.ts
@@ -1,1 +1,38 @@
-export class User {}
+import {
+  BeforeInsert,
+  BeforeUpdate,
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column('text', { unique: true })
+  username: string;
+
+  @Column('text', { unique: true })
+  email: string;
+
+  @Column('text')
+  fullName: string;
+
+  @Column('bool', { default: true })
+  isActive: boolean;
+
+  @Column('text', { select: false })
+  password: string;
+
+  @Column('text', { array: true, default: ['user'] })
+  roles: string[];
+
+  @BeforeInsert()
+  @BeforeUpdate()
+  normalizeFields() {
+    this.email = this.email.toLowerCase();
+    this.username = this.username.toLowerCase();
+  }
+}
