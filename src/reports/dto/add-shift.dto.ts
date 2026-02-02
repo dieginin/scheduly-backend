c47b1b8436caceafc9bceda2b249b7a3f32aa491
dieginin <diegobalestra9@gmail.com>
2026-02-01 17:20:41 -0800

    feat: add validation for lunchStart and lunchEnd in AddShiftDto

--- src/reports/dto/add-shift.dto.ts
+++ src/reports/dto/add-shift.dto.ts
@@ -1,6 +1,17 @@
-import { IsDateString } from 'class-validator';
+import { IsDateString, ValidateIf } from 'class-validator';
 
 export class AddShiftDto {
+  @IsDateString()
+  endDate: Date;
+
+  @IsDateString()
+  @ValidateIf((x: AddShiftDto) => x.lunchStart !== undefined)
+  lunchEnd?: Date;
+
+  @IsDateString()
+  @ValidateIf((x: AddShiftDto) => x.lunchEnd !== undefined)
+  lunchStart?: Date;
+
   @IsDateString()
   startDate: Date;
 }
