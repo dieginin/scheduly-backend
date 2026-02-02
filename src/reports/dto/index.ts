e4a2ff9c4766958a175b908e27ba96568410329c
dieginin <diegobalestra9@gmail.com>
2026-02-01 20:18:39 -0800

    refactor: remove UpdateReportDto and its export from DTO index

--- src/reports/dto/index.ts
+++ src/reports/dto/index.ts
@@ -2,5 +2,4 @@ export { AddLunchDto } from './add-lunch.dto';
 export { AddShiftDto } from './add-shift.dto';
 export { CreateReportDto } from './create-report.dto';
 export { UpdateLunchDto } from './update-lunch.dto';
-export { UpdateReportDto } from './update-report.dto';
 export { UpdateShiftDto } from './update-shift.dto';
