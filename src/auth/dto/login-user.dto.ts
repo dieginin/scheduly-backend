dea28f48e30a878fedcea2b46ef4142d40017f33
dieginin <diegobalestra9@gmail.com>
2026-02-07 23:27:26 -0800

    refactor: remove password complexity validation from LoginUserDto

--- src/auth/dto/login-user.dto.ts
+++ src/auth/dto/login-user.dto.ts
@@ -1,4 +1,4 @@
-import { IsString, Matches, MaxLength, MinLength } from 'class-validator';
+import { IsString, MaxLength, MinLength } from 'class-validator';
 
 export class LoginUserDto {
   @IsString()
@@ -8,9 +8,5 @@ export class LoginUserDto {
   @IsString()
   @MinLength(6)
   @MaxLength(50)
-  @Matches(/(?:(?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
-    message:
-      'The password must have a Uppercase, lowercase letter and a number',
-  })
   password: string;
 }
